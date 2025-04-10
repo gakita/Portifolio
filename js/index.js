cff48c41ba0a50176c8f9becb6e7f7c8ab04b051
gakita <raulgru5@gmail.com>
2025-04-10 09:36:04 -0300

    Portifolio Finalizado

---
+++ js/index.js
@@ -0,0 +1,21 @@
+const linkSistema =  document.getElementById("link_Sistema")
+
+
+linkSistema.addEventListener("click", () => {
+    window.location.href = "https://github.com/Luerfel/Gerenciador_estoque.git"
+})
+
+
+const linkGerenciador =  document.getElementById("gerenciador_senhas")
+
+
+linkGerenciador.addEventListener("click", () => {
+    window.location.href = "https://github.com/feandretta/PI3--g105-4.git"
+})
+
+const linkTravelgram =  document.getElementById("travelgram")
+
+
+linkTravelgram.addEventListener("click", () => {
+    window.location.href = "https://github.com/gakita/Travelgram.git"
+})
